bf515fcba7fd6ca90187102ac19618aef5dbc826
luisMolina95 <luisrodmolina@gmail.com>
2026-03-13 17:13:10 -0600

    fix gnu example

--- linux/c/study/gnu_server.c
+++ linux/c/study/gnu_server.c
@@ -32,6 +32,30 @@ int read_from_client(int filedes)
         return 0;
     }
 }
+
+int make_socket(uint16_t port)
+{
+    int sock;
+    struct sockaddr_in name;
+    /* Create the socket. */
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    name.sin_family = AF_INET;
+    name.sin_port = htons(port);
+    name.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(sock, (struct sockaddr *)&name, sizeof(name)) < 0)
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+    return sock;
+}
+
 int main(void)
 {
     extern int make_socket(uint16_t port);
